0ac300cfb9ecaeaceffba663b2fdd11494ea80ec
Yehia Abo El-Nga <yehia@devrok.com>
2014-09-30 18:00:11 -0700

    Make _id inclusive and exclusive

--- lib/indexable_attribute.rb
+++ lib/indexable_attribute.rb
@@ -5,18 +5,19 @@ require "active_record"
 module IndexableAttribute
   extend ActiveSupport::Concern
 	module ClassMethods
-      def indexable_attribute(attr_name, attr_list)
+      def indexable_attribute(attr_name, attr_list, has_id=true)
       	_CNST = attr_name.to_s.upcase
       	_ATTR = attr_name.to_s.downcase
+      	_ATTR_ID = has_id ? _ATTR + '_id' : _ATTR
         class_eval %Q{
         	#{_CNST} = #{attr_list.map { |i| i.to_sym }.to_s}
 
 			def #{_ATTR}
-				#{_CNST}[self[:#{_ATTR}_id]]
+				#{_CNST}[self[:#{_ATTR_ID}]]
 			end
 
 			def #{_ATTR}=(v)
-				self[:#{_ATTR}_id] = #{_CNST}.index(v.to_sym)
+				self[:#{_ATTR_ID}] = #{_CNST}.index(v.to_sym)
 			end
 	    }
       end
--- lib/indexable_attribute/version.rb
+++ lib/indexable_attribute/version.rb
@@ -1,3 +1,3 @@
 module IndexableAttribute
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 end
